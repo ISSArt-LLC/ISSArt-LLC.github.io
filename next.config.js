b33a839b38e12738ca63218e6aa4090f1cf9b3ce
Stopkin Sergey <stopkin.sergey@gmail.com>
2023-04-12 03:53:25 +0600

    Update github aciton

--- next.config.js
+++ next.config.js
@@ -1,41 +1,13 @@
-// const fs = require("fs");
-// const blogPostsFolder = "./content/blogPosts";
-
-// const getPathsForPosts = () => {
-//   return fs
-//     .readdirSync(blogPostsFolder)
-//     .map((blogName) => {
-//       const trimmedName = blogName.replace(".md", "");
-//       return {
-//         [`/blog/post/${trimmedName}`]: {
-//           page: `/blog/post/${trimmedName}`,
-//         },
-//       };
-//     })
-//     .reduce((acc, curr) => {
-//       return { ...acc, ...curr };
-//     }, {});
-// };
-
 /** @type {import('next').NextConfig} */
 module.exports = {
+  /**
+   * Disable server-based image optimization.
+   *
+   * @see https://nextjs.org/blog/next-12-3#disable-image-optimization-stable
+   */
   images: {
     unoptimized: true,
   },
   reactStrictMode: true,
   output: "export",
-  // webpack: (configuration) => {
-  //   configuration.module.rules.push({
-  //     test: /\.md$/,
-  //     use: "frontmatter-markdown-loader",
-  //   });
-  //   configuration.resolve.fallback = { fs: false };
-  //   return configuration;
-  // },
-  // async exportPathMap(defaultPathMap) {
-  //   return {
-  //     ...defaultPathMap,
-  //     ...getPathsForPosts(),
-  //   };
-  // },
 };
