111368a9bf9be12105fec181b7f552d5f1169912
e-muravev <e.muravev.js@gmail.com>
2023-04-13 12:38:28 +0700

    extract post content code into component

---
+++ components/PostContentCard/index.tsx
@@ -0,0 +1,65 @@
+import Image from "next/image";
+import { marked } from "marked";
+import { Box, Chip, Link, Stack, Typography } from "@mui/material";
+import { slugify } from "../../utils";
+
+interface PostContentCardProps {
+  frontmatter: {
+    [key: string]: any;
+  };
+  content: string;
+}
+
+export default function PostContentCard({
+  frontmatter,
+  content,
+}: PostContentCardProps) {
+  const date = new Date(frontmatter.date);
+
+  return (
+    <div className="card card-page">
+      <Typography gutterBottom variant="h3" component="div">
+        {frontmatter.title}
+      </Typography>
+      <Typography gutterBottom variant="h6" component="div">
+        {frontmatter.categories
+          ? `Categories: ` + frontmatter.categories.join(", ")
+          : ""}
+      </Typography>
+      <Typography variant="body2" color="text.secondary">
+        {`${date.getFullYear()}-${date.getMonth() + 1}-${date.getDate()}`} by{" "}
+        {frontmatter.author}
+      </Typography>
+      <div className="post-image">
+        <Box display="flex" justifyContent="center" alignItems="center">
+          <Image
+            src={
+              frontmatter?.image ? frontmatter.image : "/assets/images/logo.jpg"
+            }
+            alt=""
+            width="250"
+            height="250"
+          />
+        </Box>
+      </div>
+      <Stack direction="row" spacing={1}>
+        {frontmatter.tags
+          ? frontmatter.tags.map((tag: string) => {
+              const slug = slugify(tag);
+
+              return (
+                <Link key={tag} href={`/tag/${slug}`}>
+                  <Chip variant="outlined" clickable={true} label={`#${tag}`} />
+                </Link>
+              );
+            })
+          : "no tags"}
+      </Stack>
+
+      <div
+        className="post-body"
+        dangerouslySetInnerHTML={{ __html: marked.parse(content) }}
+      ></div>
+    </div>
+  );
+}
