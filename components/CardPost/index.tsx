4c2079117ccda92d8e5293765ca868233544750b
e-muravev <e.muravev.js@gmail.com>
2023-04-13 13:21:53 +0700

    fix tags

--- components/CardPost/index.tsx
+++ components/CardPost/index.tsx
@@ -1,16 +1,15 @@
-import Link from 'next/link'
-import { slugify } from '../../utils'
-import Image from 'next/image'
-import { Avatar, Box, Button, Chip, Stack } from '@mui/material'
-import * as React from 'react';
-import Card from '@mui/material/Card';
-import CardActions from '@mui/material/CardActions';
-import CardContent from '@mui/material/CardContent';
-import Typography from '@mui/material/Typography';
+import Link from "next/link";
+import { slugify } from "../../utils";
+import Image from "next/image";
+import { Avatar, Box, Button, Chip, Stack } from "@mui/material";
+import * as React from "react";
+import Card from "@mui/material/Card";
+import CardActions from "@mui/material/CardActions";
+import CardContent from "@mui/material/CardContent";
+import Typography from "@mui/material/Typography";
 
 export default function CardPost({ post }: any) {
-
-  const date = new Date(post.date)
+  const date = new Date(post.date);
   return (
     <Card>
       <CardContent>
@@ -18,38 +17,40 @@ export default function CardPost({ post }: any) {
           {post.title}
         </Typography>
         <Typography gutterBottom variant="h6" component="div">
-          {(post.categories) ? (`Categories: ` + post.categories.join(', ')) : ''}
+          {post.categories ? `Categories: ` + post.categories.join(", ") : ""}
         </Typography>
         <Typography variant="body1" color="text.secondary">
-          {`${date.getFullYear()}-${date.getMonth() + 1}-${date.getDate()}`} by {post.author}
+          {`${date.getFullYear()}-${date.getMonth() + 1}-${date.getDate()}`} by{" "}
+          {post.author}
         </Typography>
-        <Box
-          display="flex"
-          justifyContent="center"
-          alignItems="center">
+        <Box display="flex" justifyContent="center" alignItems="center">
           <Link href={`/blog/post/${post.slug}`}>
-            <Image
-              src={(post.image) ? post.image : '/assets/images/logo.jpg'}
-              alt=""
-              width="250"
-              height="250"
-            />
+            <Box>
+              <Image
+                src={post.image ? post.image : "/assets/images/logo.jpg"}
+                alt=""
+                width="250"
+                height="250"
+                // fill
+              />
+            </Box>
           </Link>
         </Box>
-        <Stack direction="row" spacing={1}>
-          {
-            (post.tags) ?
-              post.tags.map(
-                (tag: string) => {
-                  const slug = slugify(tag)
-                  return (
-                    <Link key={tag} href={`/tag/${slug}`}>
-                      <Chip variant="outlined" clickable={true} label={`#${tag}`} />
-                    </Link>
-                  )
-                }
-              ) : ''
-          }
+        <Stack direction="row" spacing={1} flexWrap="wrap" useFlexGap>
+          {post.tags
+            ? post.tags.map((tag: string) => {
+                const slug = slugify(tag);
+                return (
+                  <Link key={tag} href={`/tag/${slug}`}>
+                    <Chip
+                      variant="outlined"
+                      clickable={true}
+                      label={`#${tag}`}
+                    />
+                  </Link>
+                );
+              })
+            : ""}
         </Stack>
       </CardContent>
       <CardActions>
